5da239c736f2ff07bb67be870b46949ad84ed832
Mike Kenway <mike@mikekenway.com>
2025-10-31 19:20:49 -0400

    dashboard redesign. added sidebar.

--- web/src/routes/(authenticated)/route.tsx
+++ web/src/routes/(authenticated)/route.tsx
@@ -1,14 +1,21 @@
-import Header from '@/components/Header'
 import { createFileRoute } from '@tanstack/react-router'
 import { Outlet } from '@tanstack/react-router'
+import { SidebarProvider, SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
+import { Navbar } from '@/components/Navbar'
 
 export const Route = createFileRoute('/(authenticated)')({
   component: DashboardLayout,
 })
 
 function DashboardLayout() {
-  return <div>
-    <Header />
-    <Outlet />
-  </div>
+  return (
+    <SidebarProvider>
+      <Navbar />
+      <SidebarInset>
+        <div className="flex flex-1 flex-col">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
