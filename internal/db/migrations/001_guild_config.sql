be88b1d460642fc0442bfa1359441582e82ea873
Mike Kenway <mike@mikekenway.com>
2025-11-15 14:16:03 -0500

    db updates

--- internal/db/migrations/001_guild_config.sql
+++ internal/db/migrations/001_guild_config.sql
@@ -2,7 +2,7 @@
 create table guild_config (
     guild_id text primary key,
     default_vc text,
-    volume int not null default 100 check (volume between 0 and 200),
+    volume int not null default 25 check (volume between 0 and 200),
     language_filter_enabled boolean not null default false,
     updated_at timestamptz not null default now()
 );
