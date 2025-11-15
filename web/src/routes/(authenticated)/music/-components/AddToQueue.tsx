f8eb11bdbac3cb2b0a8f6ab756c9042edf8abe60
Mike Kenway <mike@mikekenway.com>
2025-11-14 19:26:28 -0500

    file cleanup

--- web/src/routes/(authenticated)/music/-components/AddToQueue.tsx
+++ web/src/routes/(authenticated)/music/-components/AddToQueue.tsx
@@ -1,6 +1,5 @@
 import { Input } from '@/components/ui/input';
 import { Field } from '@/components/ui/field';
-import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { useForm } from '@tanstack/react-form';
